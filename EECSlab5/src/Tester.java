03822373ea56f46b3590172e123450330780b04a
Gao-Xixi <77395018+Gao-Xixi@users.noreply.github.com>
2021-07-15 15:58:05 -0400

    lab5 finished

---
+++ EECSlab5/src/Tester.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Tester {
+
+    @Test
+    public void getSalary() {
+    }
+
+    @Test
+    public void setSalary() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
